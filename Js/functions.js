d51915ff586c7592fad94854e0f6df59f789fc28
MrWallace <walasrazoth@gmail.com>
2023-08-20 13:24:53 -0500

    Reubicación  de ciertos elementos y cambio en el panel izquierdo
    xd

--- Js/functions.js
+++ Js/functions.js
@@ -1,6 +1,7 @@
 $("#nuevoUsuario").click(function() {
     var nombre = $("#nombre").val(); //capturar el valor del campo con id nombre
     var identificacion = $("#identificacion").val(); //capturar el valor del campo con id identificacion
-    $('#usuarios tr:last').after('<tr><td>' + nombre + '</td><td>' + identificacion + '</td></tr>'); //agrega un nuevo registro a la tabla, después del último tr
+    var cargo = $("#cargo").val();
+    $('#usuarios tr:last').after('<tr><td>' + nombre + '</td><td>' + identificacion + '</td><td>' + cargo +'</td></tr>'); //agrega un nuevo registro a la tabla, después del último tr
     $('#nuevoUsuarioModal').modal('toggle'); //cierra el modal
 });
